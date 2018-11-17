f6ec7b879f5bd8093015f5353f801b9da9f3a5e7
Yadali <SYadali@mhc.trk>
2018-11-17 15:21:14 -0600

    elm-ui

--- src/Main.elm
+++ src/Main.elm
@@ -1,162 +1,178 @@
 module Main exposing (..)
 
-import Html exposing (..)
-import Maybe
-import Array exposing (Array)
-import Data.Difficulty exposing (..)
-import Data.Question exposing (..)
-import View.Question exposing (..)
-import Html.Attributes exposing (value)
-import Html.Events exposing (onInput, onClick)
 import Browser
-import Task
---import Json.Decode exposing (Value)
-import Http exposing(..)
-
-import Json.Decode as Decode
-import Json.Encode as Encode
-
-import Utils exposing (..)
-import Url.Builder exposing (..)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Events exposing (..)
+import Element.Font as Font
+import Element.Input as Input
+import Html exposing (Html)
 
 type alias Model
     = {
-        amount : Int
-        ,difficultLevel : DifficultyLevel
-        ,questions : Array Question
-      }
+        channelList : List String
+        , chatMessages : List ChatMessage
+    }
 
-type alias Flagsx =
-    Int
-
-init : Flagsx -> (Model, Cmd Msg)
-init intialQuestionCount =
-    ({
-        amount = intialQuestionCount -- this gets value from the index.js file
-        ,difficultLevel = defaultDiffcultyLevel
-        ,questions = Array.empty
-            -- (Array.fromList
-            --     [ 
-            --         -- this style of constructing Question unlike record style, works too, we can treat type aliases as a contructor funtion
-            --     --     Question  Nothing "Is Elm the best UI Language "           "True"   ["True", "False"] "Nothing" --["A1","A2","A3","A4","A5"]
-            --     --     ,Question Nothing "Does Elm supports Native Mobile apps " "False"   ["True", "False"] "Nothing" --["B1","B2","B3","B4","B5"]
-            --     --     ,Question Nothing "Has Elm invented by MHC "               "False"  ["True", "False"] "Nothing" --["D1","D2","D3","D4","D5"]
-
-            --         Question  Nothing "Is Elm the best UI Language "           "True"   ["True", "False"] Nothing --["A1","A2","A3","A4","A5"]
-            --         ,Question Nothing "Does Elm supports Native Mobile apps " "False"   ["True", "False"] Nothing --["B1","B2","B3","B4","B5"]
-            --         ,Question Nothing "Has Elm invented by MHC "               "False"  ["True", "False"] Nothing --["D1","D2","D3","D4","D5"]
-            --     ]
-            -- )
-    }, Cmd.none)
+type alias ChatMessage =
+    { author : String, time : String, text : String }
 
 type Msg
-    = Answer Int String
-    | UpdateAmount String
-    | ChangeDifficulty DifficultyLevel
-    | Start
-    | GetQuestions (Result Error TriviaResults)
+    = Answer Int
 
-main: Program Flagsx Model Msg
-main = 
-     Browser.element { init = init, update = update, view = view, subscriptions = always Sub.none }
- 
+init : () -> (Model, Cmd Msg)
+init _=
+    (
+        {
+            channelList = ["ellie", "elm-core", "elm-format", "elm-ui", "elm-discuss", "general", "news and links"]
+            ,chatMessages = [ 
+                                ChatMessage "Suresh Yadali" "12:30 AM" "By the way, elm-ui does away with the rather troublesome concept of margins and padding that's found in CSS. Instead, you can specify internal padding for an element, and you can also specify the spacing between its child elements. I find this much more intuitive."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                ,ChatMessage "Indira Yadali" "10:30 PM" "At first glance, this might look like a lot of code, however note that it's really straightforward, and most of it is simply layout and styling attributes. At the bottom of the function, it's very clearly stated that we have vertically arranged header, messages and footer."
+                                
+                            ]
+        }, Cmd.none)
+
 update: Msg -> Model -> (Model, Cmd Msg)
 update msg model =
-    case msg of
-        Start -> 
-            let
-                difficutlyValue =
-                    model.difficultLevel
-                        |> convertDifficutlyLevelToString
-                        |> String.toLower
-                
-                difficutlyArg =
-                            if isAnyDifficultLevel model.difficultLevel then
-                                 ""
-                            else
-                                convertDifficutlyLevelToString model.difficultLevel
-            in
-                (model,  Http.get { url = crossOrigin "https://opentdb.com" ["api.php"] [int "amount" model.amount, string "type" "boolean", string "difficulty" difficutlyArg ]
-                , expect = Http.expectJson GetQuestions rootResultDecoder })
-            
-        GetQuestions triviaResult -> 
-            let
-                questionsList = 
-                                case triviaResult of
-                                    Ok triviaResultValue ->
-                                            -- (Array.fromList
-                                            --     [
-                                            --         Question  Nothing "Is Elm the best UI Language "           "True"   ["True", "False"] Nothing --["A1","A2","A3","A4","A5"]
-                                            --         ,Question Nothing "Does Elm supports Native Mobile apps " "False"   ["True", "False"] Nothing --["B1","B2","B3","B4","B5"]
-                                            --         ,Question Nothing "Has Elm invented by MHC "               "False"  ["True", "False"] Nothing --["D1","D2","D3","D4","D5"] 
-                                            --     ]
-                                            -- )
-                                            Array.fromList triviaResultValue.results -- questions from the http.get
-                                    Err err ->
-                                            Array.empty
-            in
-            
-                ( { model | questions = questionsList } , Cmd.none )
+    (model, Cmd.none)
 
-        Answer index userSelectedAnswer ->
-        -- First style
-            (model.questions
-                |> Array.get index
-                |> Maybe.map (\q -> {q |    
-                                        userAnswer = Just userSelectedAnswer, 
-                                        --isCorrect = if (q.correct == userSelectedAnswer) then "Yes" else "No" })  -- this is when isCorrect is a String type
-                                        isCorrect = if (q.correct == userSelectedAnswer) then Just True else Just False })  -- this is when isCorrect is a Maybe Bool type
-                
-                --|> Maybe.map (\q -> {q | isCorrect = if (q.correct == userSelectedAnswer) then "Yes" else "No" })
-                |> Maybe.map (\q -> Array.set index q model.questions)
-                |> Maybe.map (\arr -> {model | questions = arr})
-                |> Maybe.withDefault model
-            , Cmd.none)
-    
-        UpdateAmount amountString ->
-            (case (String.toInt amountString) of
-                Just val ->
-                    {model | amount = val}
-            
-                Nothing ->
-                    {model | amount = 0}
-            ,sendMessage Start)
-        --    ({model | amount = Just amountString}
-        --    ,sendMessage Start)
+main: Program () Model Msg
+main = 
+     Browser.element { init = init, update = update, view = view, subscriptions = always Sub.none }
 
-        ChangeDifficulty newDiffcultyLevel ->
-            -- the commented two lines also works to send Start message back to update function, but below code is the best option
-            -- {model | difficultLevel = newDiffcultyLevel}
-            -- |> update (Start)
-            ({model | difficultLevel = newDiffcultyLevel},
-            sendMessage Start)
-               
-view: Model -> Html Msg
-view {amount, questions} =
-    let
-        qlist = (questions
-                    |> Array.indexedMap(\i q -> questionView (Answer i) q) -- Answer i creates a partial function which takes a string and btn function calls that with the string value to complete the Answer message with both int and string params
-                    --its like attaching a function to click events and call the function runtime, but here it is just a 
-                    |> Array.toList)
-                --|> List.intersperse (text " ---  ")
-        --selectedDifficultyLevel = get "Easy"
-    in
-        div []
-            [
-                input [ onInput UpdateAmount,  value (String.fromInt amount)][]
-                --, select [onInput (ChangeDifficulty << getConvertedDifficultyLevelFromString)] -- this one automatically sends selectedOptiontext value to get function
-                -- and feeds the response which is converted from string to DifficultyLevel in to ChangeDifficulty Message
-                ,select [onInput (\selectedOptionText -> ChangeDifficulty (getConvertedDifficultyLevelFromString selectedOptionText)) ] -- You can do this too
-                    (
-                        List.map (\key -> option [] [text key]) keys
-                    )
-                ,button [onClick Start][text "Start Quiz "]
-                ,div[]
-                   qlist
+view : Model -> Html Msg
+view  model =
+    layout [height fill  ] <|
+        row [ height fill, width fill,Border.glow (rgb255 244 65 65) 0 ]
+            [ channelPanel model "elm-core"
+            , chatPanel model
             ]
 
--- this is just to send a message back in to update function when there are no side-effects needed and just a way to put the message back in to update function
-sendMessage : msg -> Cmd msg
-sendMessage msg =
-    Task.succeed msg
-        |> Task.perform identity -- dont know what is identity, find out.     
+channelPanel : Model -> String -> Element msg
+channelPanel {channelList} activeChannel =
+    let
+        activeChannelAttrs =
+            [ Background.color <| rgb255 117 179 201, Font.bold ]
+
+        channelAttrs =
+            [ paddingXY 15 5]
+
+        channelEl channel =
+            el -- div
+                ( 
+                    ([width fill] ) ++ --concatinating two lists is just using ++
+                    if channel == activeChannel then
+                        channelAttrs ++ activeChannelAttrs
+                    else
+                        channelAttrs
+                )
+                    <|
+                        el [] -- div
+                            <| text ("# " ++ channel)
+
+            -- el
+            --     (if channel == activeChannel then
+            --         activeChannelAttrs ++ channelAttrs
+            --      else
+            --         channelAttrs
+            --     )
+            -- <|
+            --     text ("# " ++ channel)
+    in
+    column
+        [ height fill
+        , width <| fillPortion 1
+        , paddingXY 0 30
+        , Background.color <| rgb255 92 99 118
+        , Font.color <| rgb255 255 255 255
+        ]
+        <|
+            List.map channelEl channelList
+
+messageEntry message =
+                    column [ ] 
+                        
+                        [
+                            row [width fill , Border.widthEach { bottom = 1, top = 0, left = 0, right = 0 }] [
+                          el [ Font.bold ] <| text message.author
+                        , el [ Font.bold, alignRight ] <| text message.time ]
+                        , paragraph [alignLeft,  paddingEach { bottom = 0, top = 10, left = 0, right = 0 }] [ text message.text ]
+                        ]                    
+            -- column [ width fill, spacingXY 0 5 ]
+            --     [ 
+            --     ]
+
+chatPanel : Model -> Element msg
+chatPanel {chatMessages}=
+    column [ height fill, width <| fillPortion 5]
+    [
+        row [width fill, paddingXY 20 10
+                , Border.widthEach { bottom = 1, top = 0, left = 0, right = 0 }
+                , Border.color <| rgb255 200 200 200]
+            [
+                el [] <| text "Left"
+                , Input.button
+                    [ padding 5
+                    , alignRight
+                    , Border.width 1
+                    , Border.rounded 3
+                    , Border.color <| rgb255 200 200 200
+                    ]
+                    { onPress = Nothing
+                    , label = text "Search"
+                    }
+            ]
+        --,column [Background.color <| rgb255  204 201 201, height fill, padding 20,width fill] [ text "asdf"]
+        ,column [ paddingXY 100 3, spacingXY 0 5, scrollbarY] <|
+                List.map messageEntry chatMessages
+        ,el [ alignBottom, padding 20, width fill ] <|
+                row
+                    [ spacingXY 2 0
+                    , width fill
+                    , Border.width 2
+                    , Border.rounded 4
+                    , Border.color <| rgb255 200 200 200
+                    ]
+                    [ el
+                        [ padding 5
+                        , Border.widthEach { right = 2, left = 0, top = 0, bottom = 0 }
+                        , Border.color <| rgb255 200 200 200
+                        , mouseOver [ Background.color <| rgb255 86 182 139 ]
+                        ]
+                      <|
+                        text "+"
+                    , el [ Background.color <| rgb255 255 255 255 ] none
+                    ]
+    ]
+
+
+
+-- channelPanel : Model -> Element msg
+-- channelPanel {channelList} =
+--     column
+--         [ height fill
+--         , width <| fillPortion 1
+--         , paddingXY 0 10
+--         , Background.color <| rgb255 255 114 114
+--         , Font.color <| rgb255 255 255 255
+--         ]
+--         (List.map (\c -> el [paddingXY 15 5, alignLeft, Background.color <| rgb255 117 179 201, Font.bold] <| text ("#" ++ c )) channelList)
+
+
+
+
